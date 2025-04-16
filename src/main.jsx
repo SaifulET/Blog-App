1368fa1c6450ced3fd3035598069f40c9c1d253b
saiful <si912999@gmail.com>
2025-04-16 10:54:26 +0600

    end

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import axios from 'axios';
-axios.defaults.baseURL = 'https://blog-app-backend-indol.vercel.app/api';
-// axios.defaults.baseURL = 'http://localhost:5000/api';
+// axios.defaults.baseURL = 'https://blog-app-backend-indol.vercel.app/api';
+axios.defaults.baseURL = 'http://localhost:5000/api';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
